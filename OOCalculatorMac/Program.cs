4113509250b231c8c42731eaaa998793cefa46a0
Syed <ali56syed@gmail.com>
2017-12-09 02:01:21 -0600

    Initial Check In

---
+++ OOCalculatorMac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOCalculatorMac
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
